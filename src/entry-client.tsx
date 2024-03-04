87952a1df74a02aec77fea211363c8fc305a625e
HMBond <hubertusmariabond@gmail.com>
2024-03-04 15:28:41 +0100

    fix images to be includes in the netlify assets

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,3 +1,5 @@
 import { mount, StartClient } from "solid-start/entry-client";
+import "../public/background.jpg";
+import "../public/banner-o-casey-s.png";
 
 mount(() => <StartClient />, document);
